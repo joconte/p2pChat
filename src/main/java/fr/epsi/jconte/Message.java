1d531b692c181ed99a38240ad644b4b18cb56cc9
joconte <etud@contejonathan.com>
2019-12-12 09:45:25 +0100

    first commit

---
+++ src/main/java/fr/epsi/jconte/Message.java
@@ -0,0 +1,36 @@
+package fr.epsi.jconte;
+
+/**
+ * Describes a message that we receive. Contains information about the sender, and the message String
+ */
+public class Message {
+    private String msg;
+    private Sender mSender;
+
+    /**
+     * Create a new Message
+     * @param msg The String that we receive or send
+     * @param mSender Details about the person we send to
+     */
+    public Message(String msg, Sender mSender) {
+        this.msg = msg;
+        this.mSender = mSender;
+    }
+
+    public String getMessage() {
+        return msg;
+    }
+
+    public void setMessage(String m) {
+        msg = m;
+    }
+
+    public Sender getSender() {
+        return mSender;
+    }
+
+    @Override
+    public String toString() {
+        return mSender.getName() + ": " + msg;
+    }
+}
